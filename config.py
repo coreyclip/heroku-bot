5777ce227cf996225b6a6d198e945accf5ce6623
corey clippinger <coreyclip@gmail.com>
2018-03-06 20:57:58 -0800

    config

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "vHMWGD8V0UaQ8il5kLViWCbDU"
+consumer_secret = "wXt38St5uYMqUUZkirGXDsaREEAlqs2eYqm65NdFTbSKPMMale"
+access_token = "238703549-XipAOmhnwDxvIkU9PYJlWVb8F3EEhkggQmewQZrI"
+access_token_secret = "9Aok6oBEqtzRil83kiPRai7QI95LqnCySv3rP9SKPFFFa"
